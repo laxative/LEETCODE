1bec49578d345bd806248080fb0c7025394b1e44
Yao-Hsien Hsieh <laxative0612@gmail.com>
2021-01-02 17:16:26 +0800

    feat(Q1): implement twoSum function & fix test case

--- Q1-TwoSum/test/twosum.test.js
+++ Q1-TwoSum/test/twosum.test.js
@@ -3,14 +3,14 @@ const twoSum = require('../twosum');
 
 describe('twosum function test', () => {
   it('leetcode example 1 test', () => {
-    expect(twoSum([2,7,11,15], 9)).to.be.eql([0, 1]);
+    expect(twoSum([2,7,11,15], 9)).to.have.members([0, 1]);
   });
 
   it('leetcode example 2 test', () => {
-    expect(twoSum([3,2,4], 6)).to.be.eql([1, 2]);
+    expect(twoSum([3,2,4], 6)).to.have.members([1, 2]);
   });
 
   it('leetcode example 3 test', () => {
-    expect(twoSum([3,3], 6)).to.be.eql([0, 1]);
+    expect(twoSum([3,3], 6)).to.have.members([0, 1]);
   });
 })
--- Q1-TwoSum/twosum.js
+++ Q1-TwoSum/twosum.js
@@ -1,10 +1,26 @@
 /**
+ * First find out the diff of current num to target,
+ * then using Array.prototype.indexOf to find the diff num index
+ * if exist (not equal to -1), return current num index and diff num index
+ *
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  return [0, 1];
+  let ans = null;
+
+  for (let i = 0; i < nums.length; ++i) {
+    let diff = target - nums[i];
+    let diffIndex = nums.indexOf(diff);
+
+    if (diffIndex !== -1 && diffIndex !== i) {
+      ans = [i, diffIndex];
+      break;
+    }
+  }
+
+  return ans;
 }
 
 module.exports = twoSum;
