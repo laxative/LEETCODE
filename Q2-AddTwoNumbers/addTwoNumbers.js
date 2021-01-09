dbeed9f6f118763e4a3de722656ee63924be3d2b
Yao-Hsien Hsieh <laxative0612@gmail.com>
2021-01-09 19:43:18 +0800

    feat(Q2): implement addTwoNumbers function

--- Q2-AddTwoNumbers/addTwoNumbers.js
+++ Q2-AddTwoNumbers/addTwoNumbers.js
@@ -12,7 +12,23 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    return new ListNode(0);
+  let ans = new ListNode();
+  let head = ans;
+  let add = 0;
+  while (l1 !== null || l2 !== null || add !== 0) {
+    let cal = ((l1 ? l1.val : 0) + (l2 ? l2.val : 0) + add);
+    ans.val = cal % 10;
+    add = Math.floor(cal / 10);
+
+    l1 = l1 ? l1.next : l1;
+    l2 = l2 ? l2.next : l2;
+    if (l1 || l2 || add) {
+      ans.next = new ListNode();
+      ans = ans.next;
+    }
+  }
+
+  return head;
 };
 
 module.exports = addTwoNumbers;
