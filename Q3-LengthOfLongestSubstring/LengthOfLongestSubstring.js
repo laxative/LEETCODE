79cb6a4879c6413fcaa6b0313ef6b4944f0864f2
Yao Hsien Hsieh <laxative0612@gmail.com>
2021-01-21 18:32:31 +0800

    feat(Q3): implement question 3 algorithm

--- Q3-LengthOfLongestSubstring/LengthOfLongestSubstring.js
+++ Q3-LengthOfLongestSubstring/LengthOfLongestSubstring.js
@@ -2,8 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    return 0;
+var lengthOfLongestSubstring = function (s) {
+    if (s.length === 1) return 1;
+
+    let maxLength = 0;
+    for (let startIndex = 0; startIndex < s.length; startIndex++) {
+        let charUsed = {};
+        charUsed[s[startIndex]] = true;
+        for (let endIndex = startIndex + 1; endIndex < s.length; endIndex++) {
+            if (!!charUsed[s[endIndex]]) {
+                maxLength = Math.max(maxLength, endIndex - startIndex);
+                break;
+            }
+            charUsed[s[endIndex]] = true;
+        }
+    }
+    return maxLength;
 };
 
 module.exports = lengthOfLongestSubstring;
