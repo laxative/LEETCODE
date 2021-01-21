6e8dd640cc70dfc423da6f6cdf6218cbaea56b7e
Yao-Hsien Hsieh <laxative0612@gmail.com>
2021-01-21 20:42:32 +0800

    refactor(Q3): change algorithm to dynamic programming

--- Q3-LengthOfLongestSubstring/LengthOfLongestSubstring.js
+++ Q3-LengthOfLongestSubstring/LengthOfLongestSubstring.js
@@ -3,23 +3,38 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    if (s.length === 1) return 1;
+  // the condition that directly return answer
+  if (s.length <= 1) return s.length;
 
-    let maxLength = 0;
-    for (let startIndex = 0; startIndex < s.length; startIndex++) {
-        let charUsed = {};
-        charUsed[s[startIndex]] = true;
-        for (let endIndex = startIndex + 1; endIndex < s.length; endIndex++) {
-            if (!!charUsed[s[endIndex]]) {
-                maxLength = Math.max(maxLength, endIndex - startIndex);
-                break;
-            }
-            charUsed[s[endIndex]] = true;
-            maxLength = Math.max(maxLength, endIndex - startIndex + 1);
-        }
-        
+  let maxLength = 0;
+  /** 
+   * recording character place in string, if appear over one time,
+   * record the last position
+   * @type {Object<string, number>}
+   */
+  let charMap = {};
+  /**
+   * iterator is used for search substring end index
+   * startIndex is used for start substring index
+   * 
+   * if the character is not exist in char map data, save it position in data and 
+   * update maxLength
+   * if exist, update startIndex value to the character position + 1
+   * (because substring cannot exist the same char)
+   * 
+   * Using "abcabcbb" for example
+   * if iterator = 3(a), now charMap = {"a": 0, "b": 1, "c": 2}
+   * startIndex will update to 1 (now substring = "bca")
+   * and update the maxLength with the longer one ("abc" vs "bca")
+   */
+  for (let iterator = 0, startIndex = 0; iterator < s.length; ++iterator) {
+    if (charMap[s[iterator]] !== undefined) {
+      startIndex = Math.max(startIndex, charMap[s[iterator]] + 1);
     }
-    return maxLength;
+    charMap[s[iterator]] = iterator;
+    maxLength = Math.max(maxLength, iterator - startIndex + 1);
+  }
+  return maxLength;
 };
 
 module.exports = lengthOfLongestSubstring;
