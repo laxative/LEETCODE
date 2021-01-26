97910a310e6da34e9caeadecc8886caa0b8b5d88
Yao-Hsien Hsieh <laxative0612@gmail.com>
2021-01-26 19:46:47 +0800

    feat(Q4): implement question 4 algorithm

--- Q4-FIndMedianSortedArrays/FindMedianSortedArrays.js
+++ Q4-FIndMedianSortedArrays/FindMedianSortedArrays.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  return 0;
+  let sortedNums = [];
+  for (let i = 0, j = 0; i + j < nums1.length + nums2.length;) {
+    if (nums1[i] <= nums2[j] || nums2[j] === undefined) {
+      sortedNums.push(nums1[i]);
+      i++;
+    } else {
+      sortedNums.push(nums2[j]);
+      j++;
+    }
+  }
+
+  return sortedNums.length % 2 === 0 ?
+    (sortedNums[sortedNums.length / 2] + sortedNums[sortedNums.length / 2 - 1]) / 2 :
+    sortedNums[Math.floor(sortedNums.length / 2)]
 };
 
 module.exports = findMedianSortedArrays
