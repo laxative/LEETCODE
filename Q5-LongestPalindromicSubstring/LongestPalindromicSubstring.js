8e5bca0bb93612273cc258248197e89ca5df91a8
Yao-Hsien Hsieh <laxative0612@gmail.com>
2021-03-15 23:58:53 +0800

    feat(Q5): Implement leetcode question 5
    Refer to Manacher’s Algorithm

--- Q5-LongestPalindromicSubstring/LongestPalindromicSubstring.js
+++ Q5-LongestPalindromicSubstring/LongestPalindromicSubstring.js
@@ -4,7 +4,33 @@
  */
 
 var longestPalindrome = function (s) {
-  return '';
+  let symbolString = '';
+  for (let i = 0; i < s.length; ++i) {
+    symbolString += `#${s[i]}`;
+  }
+  symbolString += '#';
+
+  let radius = new Array(symbolString.length).fill(0);
+
+  for (let center = 1; center < symbolString.length - 1; ++center) {
+    let counter = 0;
+    for (let dis = 1; center - dis >= 0 && center + dis < symbolString.length; ++dis) {
+      if (symbolString[center - dis] == symbolString[center + dis]) {
+        counter++;
+      }
+      else {
+        break;
+      }
+    }
+    radius[center] = counter;
+  }
+
+  const maxIndex = radius.indexOf(Math.max(...radius));
+  const start = (maxIndex - radius[maxIndex]) / 2;
+
+  let string = s.slice(start, start + radius[maxIndex])
+
+  return string;
 }
 
 module.exports = longestPalindrome;
