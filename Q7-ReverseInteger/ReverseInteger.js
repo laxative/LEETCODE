17c6a1df0515acb1a43a52e33f3fb0a8075d47b1
Yao-Hsien Hsieh <laxative0612@gmail.com>
2021-03-20 19:12:47 +0800

    feat(Q7): implement question 7 algorithm

--- Q7-ReverseInteger/ReverseInteger.js
+++ Q7-ReverseInteger/ReverseInteger.js
@@ -2,8 +2,27 @@
  * @param {number} x
  * @return {number}
  */
- var reverse = function(x) {
-    return x;
+var reverse = function (x) {
+  const limit = 2147483648;
+
+  let isNegative = x < 0;
+  let splitNumber = x.toString().split(".");
+
+  let number = isNegative ? splitNumber[0].slice(1) : splitNumber[0];
+  let decimal = splitNumber[1] ? splitNumber[1] : "0";
+
+  let reverseNumber = number.split("").reverse().join("");
+  let reverseDecimal = decimal.split("").reverse().join("");
+
+  let answer = parseFloat(`${reverseNumber}.${reverseDecimal}`);
+
+  if (answer >= limit) {
+    return 0;
+  } else {
+    if (isNegative) answer *= -1;
+
+    return answer;
+  }
 };
 
 module.exports = reverse;
